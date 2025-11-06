8f26e7b8e46c7a8393a6b3441130dd25cbfb2d17
Kim Jiah <2024184004@tukorea.ac.kr>
2025-11-06 17:21:43 +0900

    좀비가 공과 충돌시 크기가 줄고 삭제되는 시스템 구현

--- zombie.py
+++ zombie.py
@@ -33,6 +33,7 @@ class Zombie:
         self.load_images()
         self.frame = random.randint(0, 9)
         self.dir = random.choice([-1,1])
+        self.size = 2
 
 
     def get_bb(self):
@@ -51,9 +52,9 @@ class Zombie:
 
     def draw(self):
         if self.dir < 0:
-            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 100 * self.size, 100 * self.size)
         else:
-            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 100 * self.size, 100 * self.size)
         draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
@@ -61,5 +62,9 @@ class Zombie:
 
     def handle_collision(self, group, other):
         if group == 'zombie:ball':
-            pass
+            self.size -= 1
+            if self.size == 0:
+                game_world.remove_object(self)
+            else:
+                self.y -= 50
 
